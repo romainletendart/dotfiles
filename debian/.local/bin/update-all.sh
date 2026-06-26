c5f277d758814a01fc6ae0c87c3af3fffa8838dd
Romain Létendart <romain.letendart@wifirst.fr>
2026-06-26 21:08:05 +0200

    Add cargo updates to update-all.sh

--- debian/.local/bin/update-all.sh
+++ debian/.local/bin/update-all.sh
@@ -7,9 +7,16 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 echo "Updating firmware..."
 sudo -- fwupdmgr refresh --force && fwupdmgr update
 
+echo ""
 echo "Updating system packages..."
 sudo -- bash -c "apt update && apt upgrade"
 
-echo "Updating manually installed system packages..."
+echo ""
+echo "Updating manually installed debian packages..."
 "$SCRIPT_DIR"/update-extra-package-ghostty.sh
 "$SCRIPT_DIR"/update-extra-package-neovim.sh
+
+echo ""
+echo "Updating manually installed cargo packages"
+# cargo install --list | grep --extended-regexp --only-matching '^[^ ]+' | xargs cargo install
+cargo-install-update install-update --all
